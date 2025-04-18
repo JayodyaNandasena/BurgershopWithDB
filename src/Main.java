a93f08b4d6a68a346810b28b0dd9d7b843cbebd3
Jayodya Nandasena <jayodyanandasena@gmail.com>
2025-04-18 14:06:19 +0530

    add connection handling in Main class to verify MySQL connectivity

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import db.DBConnection;
 import view.MainMenu;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
+        Connection conn = DBConnection.getConnection();
+        if (conn != null) {
+            System.out.println("Connected to MySQL!");
+        } else {
+            System.out.println("Failed to connect.");
+        }
+
         new MainMenu().setVisible(true);
     }
 }
