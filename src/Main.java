673319f81118bb9ba6a60dcb842dd8bf71ca746d
Jayodya Nandasena <jayodyanandasena@gmail.com>
2025-04-19 00:55:04 +0530

    refactor: remove database connection logic from Main class to simplify startup process

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,6 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DBConnection.getConnection();
-        if (conn != null) {
-            System.out.println("Connected to MySQL!");
-        } else {
-            System.out.println("Failed to connect.");
-        }
-
         new MainMenu().setVisible(true);
     }
 }
